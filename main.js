b6458506b026409ebe3a50938a75f41af7130349
TheBlurrypixel <mgochoco@yahoo.com>
2019-03-11 22:51:45 -0700

    Refactor

--- main.js
+++ main.js
@@ -5,8 +5,6 @@ var fs = require('fs')
 var jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
-var directory;
-
 function myTrim(x) { return x.replace(/^\s+|\s+$/gm,''); }
 
 function searchStringInArray (str, strArray) {
@@ -75,7 +73,7 @@ function processDefsText(inText) {
 			if(newlineIndex > -1) {
 				// determine if we have a Bitmap or a Sprite
 				// previouse newline
-				var previousNewLineIndex = inText.substring(openBracketIndex, newlineIndex).lastIndexOf('\n');
+				var previousNewLineIndex = inText.substring(openBracketIndex, newlineIndex).lastIndexOf('\n') + openBracketIndex;
 				var bitmapOrSpriteSnippet = inText.substring(previousNewLineIndex, newlineIndex);
 
 				// search for Bitmap or Sprite
@@ -84,11 +82,8 @@ function processDefsText(inText) {
 					var nextNewLineIndex = inText.indexOf('\n', newlineIndex+1);
 					if(nextNewLineIndex > -1) {
 						if(inText.substring(newlineIndex+1, nextNewLineIndex).search('p\.nominalBounds') > -1) {
-							// console.log("We have a Bitmap");
 							var resObj = { type: "Bitmap", def: inText.substring(openBracketIndex, nextNewLineIndex) };
-
 							defsObj[resObj.def.match(/(?:\(lib\.)(\w+)\b/)[1]] = resObj;
-							// defsArray.push(resObj);
 							openBracketIndex = inText.indexOf('(', nextNewLineIndex+1);
 						}
 					}
@@ -97,7 +92,6 @@ function processDefsText(inText) {
 					// we have a sprite. find the next newlineIndex
 					// console.log("We have a sprite");
 					var resObj = { type: "Sprite", def: inText.substring(openBracketIndex, newlineIndex) };
-//					resObj.name = resObj.def.match(/(?:\(lib\.)(\w+)\b/)[1];
 					resObj.spriteSheet = resObj.def.match(/(?:initialize\(ss\[\")(.+)(\"\])/)[1];
 
 					var stopFrameIndex = resObj.def.search(/\bthis\.gotoAndStop\b/);
@@ -135,7 +129,10 @@ function processDefsText(inText) {
 function processJSON(inFiles, inIndexFiles) {
   if( ((inFiles) && (inFiles.length > 0)) && ((inIndexFiles) && (inIndexFiles.length > 0)) ) {
     var isWin = process.platform === "win32";
-    directory = isWin ? inFiles[0].substring(0, inFiles[0].lastIndexOf("\\")) : inFiles[0].substring(0, inFiles[0].lastIndexOf("\/"));
+		var jsonDirectory = isWin ? inFiles[0].substring(0, inFiles[0].lastIndexOf("\\")) : inFiles[0].substring(0, inFiles[0].lastIndexOf("\/"));
+		var indexDirectory = isWin ? inIndexFiles[0].substring(0, inIndexFiles[0].lastIndexOf("\\")) : inIndexFiles[0].substring(0, inIndexFiles[0].lastIndexOf("\/"));
+
+		var relativeJsonDir = path.relative(indexDirectory, jsonDirectory);
 
     var html_string = fs.readFileSync(inIndexFiles[0], 'utf8');
     var found = html_string.match(/<html>/g);
@@ -157,22 +154,22 @@ function processJSON(inFiles, inIndexFiles) {
 	  	baseName = baseName.lastIndexOf(".") > 0 ? baseName.substring(0,baseName.lastIndexOf(".")) : baseName;
 
 			// get number of properties
-	  	var frameIndex = 0;
 	  	var testObj = obj.frames;
 	  	var frameRectArray = [];
-			var frameObjArray = [];
+			var newSymbolsArray = [];
 			var tempObj = { name: baseName, frames: new Array() }
 	  	for (var key in testObj) {
+				var frameIndex = 0;
 	  		if (testObj.hasOwnProperty(key)) {
 					tempObj.frames.push([testObj[key].frame.x, testObj[key].frame.y, testObj[key].frame.w, testObj[key].frame.h]);
-					var symbolDef = "(lib." + key + " = function() {\nthis.spriteSheet = ss[\"" + baseName + "\"];\nthis.gotoAndStop(" + frameIndex + ");\n}).prototype = p = new cjs.Sprite();\n\n";
+					var symbolDef = "(lib." + key + " = function() {\n\tthis.initialize(ss[\"" + baseName + "\"]);\n\tthis.gotoAndStop(" + frameIndex + ");\n}).prototype = p = new cjs.Sprite();";
 
-					frameObjArray.push({ name: key, symbolDef: symbolDef });
+					newSymbolsArray.push({ name: key, symbolDef: symbolDef });
 					frameIndex++;
 	  		}
 	  	}
 			outSSMetaDataObjArray.push(tempObj);
-			inSSMetaDataObjArray.push(frameObjArray);
+			inSSMetaDataObjArray.push({imagePath: obj.meta.image, newSymbols: newSymbolsArray});
 		});
 
     // find script that hold lib.ssMetaData
@@ -232,12 +229,12 @@ function processJSON(inFiles, inIndexFiles) {
 			// for each new symbolDef find the old and replace it
 			for(var i=0; i<outSSMetaDataObjArray.length; i++) {
 				for(var j=0; j<outSSMetaDataObjArray[i].frames.length; j++) {
-					var name = inSSMetaDataObjArray[i][j].name;
+					var name = inSSMetaDataObjArray[i].newSymbols[j].name;
 
 					// find this name in the defs Obj
 					if(defs.hasOwnProperty(name)) {
 						var usedSS = defs[name].spriteSheet;
-						defs[name].def = inSSMetaDataObjArray[i][j].symbolDef;
+						defs[name].def = inSSMetaDataObjArray[i].newSymbols[j].symbolDef;
 						defs[name].spriteSheet = outSSMetaDataObjArray[i].name;
 
 						if(usedSSObj.hasOwnProperty(usedSS)) {
@@ -250,6 +247,57 @@ function processJSON(inFiles, inIndexFiles) {
 			}
 			// Now we should had updated the defs array to be up to date
 			// And we have an array of items to remove from the manifest
+			// get the lib.properties statement
+			var manifestFound = false;
+			var newManifest;
+			var libPropertiesStartIndex = scriptText.indexOf("lib.properties =");
+			var libPropertiesEndIndex = -1;
+			var libsPropsObj;
+			if(libPropertiesStartIndex > -1) {
+				// find the beginBrace
+				libPropertiesEndIndex = scriptText.indexOf(';', libPropertiesStartIndex);
+				var libPropOpenBraceIndex = scriptText.indexOf('{', libPropertiesStartIndex);
+				if(libPropOpenBraceIndex > -1) {
+					// find the closing brace
+					var libPropCloseBraceIndex = findEndingBrace(scriptText, libPropOpenBraceIndex, "{}");
+					if(libPropCloseBraceIndex > -1) {
+						libsPropsObj = eval("(" + scriptText.substring(libPropOpenBraceIndex, libPropCloseBraceIndex+1) + ")");
+
+						if(libsPropsObj && libsPropsObj.manifest) {
+							// go through manifest and remove unused SpriteSheets
+							newManifest = libsPropsObj.manifest.filter( (item) => !removeFromManifestArray.includes(item.id) );
+
+							// now add the new spriteSheets from the JSON
+							for(var i=0; i<outSSMetaDataObjArray.length; i++) {
+								newManifest.push({ src: relativeJsonDir + "/" + inSSMetaDataObjArray[i].imagePath, id: outSSMetaDataObjArray[i].name });
+							}
+							manifestFound = true;
+						}
+					}
+				}
+			}
+
+			// now get the full libProps string
+			var libPropsString = scriptText.substring(libPropertiesStartIndex, libPropertiesEndIndex+1);
+
+			function manItemToString(inObj) {
+				var inObjKeys = Object.keys(inObj);
+				return "{" + inObjKeys.reduce( (tot, cur, curInd) => {
+					var isString = typeof(inObj[cur]) == "string";
+					return ((curInd > 0) ? tot + ", " : "") + cur + ":" + (isString ? "\"" : "") + inObj[cur] + (isString ? "\"" : "");
+				}, "") + "}";
+			}
+
+			var newManifestString = newManifest.reduce( (newManTotal, newManCurrent, newManCurInd) => {
+				return ( (newManCurInd > 0) ? newManTotal + ",\n" : "" ) + "\t\t" + manItemToString(newManCurrent);
+			}, "");
+
+			// replace the manigest with our new one
+			var newLibPropsString = libPropsString.replace(/(manifest\: \[)([\s.\S][^\]]*)(\])/, "$1\n" + newManifestString + "\n\t$3");
+
+			// go through removeFromManifestArray and remove unused spritesheets from libssMetadataArray
+			libssMetadataArray = libssMetadataArray.filter( (item) => !removeFromManifestArray.includes(item.name) );
+
 			// write out a new ssMetaData and new symbol defs
 			var accumString = libssMetadataArray.reduce( (total, current, currentIndex) => {
 				return ( ((currentIndex > 0) ? total + ",\n" : "") + "\t{ name:\"" + current.name + "\", frames: [" + current.frames.reduce( (framesTotal, framesCurrent, framesCurrentIndex) => {
@@ -258,14 +306,34 @@ function processJSON(inFiles, inIndexFiles) {
 			}, "");
 			var newLibSSMetadataString = "lib.ssMetadata = [\n" + accumString + "\n];\n"
 
+			var newSymbolDefsString = defsKeys.reduce( (defTotal, defCurrent, defCurrentIndex) => {
+				return ( (defCurrentIndex > 0) ? defTotal + "\n" : "" ) + defs[defCurrent].def;
+			}, "");
+
 			var newScriptText = (scriptText.substring(0, startOfLibMeta)
 				+ newLibSSMetadataString
-				+ scriptText.substring(endOfLibMeta+1)
+				+ "\/\/ symbols:\n"
+				+ newSymbolDefsString
+				+ "\/\/ helper functions:\n"
+				+ scriptText.substring(endIndex, libPropertiesStartIndex)
+				+ newLibPropsString
+				+ scriptText.substring(libPropertiesEndIndex+1)
 			);
-			console.log(newScriptText);
 
-			// console.log("new libssMetaData: \n" + libssMetadataArray + "\n");
-			// console.log("symbol defs: \n" + outSymbolDefs + "\n");
+			var par = scriptInputs[libMetadataIndex].parentNode;
+			var elmnt = dom.window.document.createElement("script");
+			var textnode = dom.window.document.createTextNode(newScriptText);
+			elmnt.appendChild(textnode);
+			par.replaceChild(elmnt, scriptInputs[libMetadataIndex]);
+
+			var outputHtml;
+			if(found) {
+				outputHtml = "<!DOCTYPE html>\n" + dom.window.document.getElementsByTagName('html')[0].outerHTML;
+			}
+			else {
+				outputHtml = dom.window.document.getElementsByTagName('head')[0].innerHTML + dom.window.document.getElementsByTagName('body')[0].innerHTML;
+			}
+			fs.writeFileSync(inIndexFiles[0], outputHtml);
     }
   }
   else {
